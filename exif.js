a406e3555a6e14d9708fe848e3fce9c559222a57
Rungsikorn Rungsikavanich <rungsikorn@icloud.com>
2017-11-30 19:25:24 +0700

    fix(oreitation) fix

--- exif.js
+++ exif.js
@@ -1,5 +1,4 @@
-import exif from "exif-js";
-
+import exif from 'exif-js'
 
 export var EXIF_TRANSFORMS = {
   1: { rotate: 0, flip: false },
@@ -9,95 +8,116 @@ export var EXIF_TRANSFORMS = {
   5: { rotate: Math.PI * 1.5, flip: true },
   6: { rotate: Math.PI * 0.5, flip: false },
   7: { rotate: Math.PI * 0.5, flip: true },
-  8: { rotate: Math.PI * 1.5, flip: false },
-};
+  8: { rotate: Math.PI * 1.5, flip: false }
+}
 
+export default (async function resolveExif(
+  image,
+  maxWidth = 800,
+  type = 'url',
+  quality = 0.95
+) {
+  if (!HTMLCanvasElement.prototype.toBlob) require('blueimp-canvas-to-blob')
 
-export default async function resolveExif(image, maxWidth = 800, type = 'url', quality = 0.95) {
-  if (!HTMLCanvasElement.prototype.toBlob) require("blueimp-canvas-to-blob")
-
-  if (typeof image == "string") {
-    image = await getImageFromUrl(image);
+  if (typeof image == 'string') {
+    image = await getImageFromUrl(image)
   }
 
-  let url = await new Promise(function (resolve, reject) {
-    exif.getData(image, function () {
-      let orientation = exif.getTag(this, "Orientation");
-      let canvas = getCanvasForImage(image, maxWidth);
-      let w = canvas.width;
-      let h = canvas.height;
+  let url = await new Promise(function(resolve, reject) {
+    exif.getData(image, function() {
+      let orientation = exif.getTag(this, 'Orientation')
+      let canvas = getCanvasForImage(image, maxWidth)
+      let w = canvas.width
+      let h = canvas.height
+      let transformX = 0
+      let transformY = 0
 
-      let isPortrait = image.height > image.width
-      let isLarger = canvas.width < image.width;
-      
-      let transformX = !isLarger ? 0 : -w / 2
-      let transformY = !isLarger ? 0 : -h / 2
+      let isPortrait = orientation === "1"
 
+      console.log(image.width, canvas.width)
       if (orientation > 4) {
         let temp = canvas.width;
         canvas.width = canvas.height;
         canvas.height = temp;
       }
 
-      let ctx = canvas.getContext("2d");
+      console.log(image.width, canvas.width)
+      transformX = isPortrait ? 0 : -w / 2
+      transformY = isPortrait ? 0 : -h / 2
+        console.log(transformX, transformY)
 
-      exifTransformCanvas(ctx, orientation);
-      ctx.drawImage(image, 0, 0, image.width, image.height, transformX, transformY, w, h);
+      let ctx = canvas.getContext('2d')
+
+      exifTransformCanvas(ctx, orientation)
+      ctx.drawImage(
+        image,
+        0,
+        0,
+        image.width,
+        image.height,
+        transformX,
+        transformY,
+        w,
+        h
+      )
+      console.log('done')
       if (type === 'blob')
-        canvas.toBlob(function (blob) {
-          resolve(blob);
-        }, 'image/jpeg', quality);
+        canvas.toBlob(
+          function(blob) {
+            resolve(blob)
+          },
+          'image/jpeg',
+          quality
+        )
       else {
-        resolve(canvas.toDataURL());
+        resolve(canvas.toDataURL())
       }
+    })
+  })
 
-    });
-  });
-
-  return url;
-}
-
+  return url
+})
 
 export function getImageFromUrl(url) {
-  return new Promise(function (resolve, reject) {
-    let image = new Image();
-    image.crossOrigin = "anonymous";
-    image.onload = function () { resolve(image); };
-    image.onerror = function (e) { reject(e); };
-    image.src = url;
-  });
+  return new Promise(function(resolve, reject) {
+    let image = new Image()
+    image.crossOrigin = 'anonymous'
+    image.onload = function() {
+      resolve(image)
+    }
+    image.onerror = function(e) {
+      reject(e)
+    }
+    image.src = url
+  })
 }
-
 
 export function getCanvasForImage(image, maxWidth) {
-  let canvas = document.createElement("canvas");
-  let w = image.width;
-  let h = image.height;
+  let canvas = document.createElement('canvas')
+  let w = image.width
+  let h = image.height
 
   if (maxWidth && w > maxWidth) {
-    let ratio = w / h;
-    w = maxWidth;
-    h = w / ratio;
+    let ratio = w / h
+    w = maxWidth
+    h = w / ratio
   }
-  canvas.width = w;
-  canvas.height = h;
-  return canvas;
+  canvas.width = w
+  canvas.height = h
+  return canvas
 }
 
-
 export function exifTransformCanvas(ctx, orientation) {
-  let transform = EXIF_TRANSFORMS[orientation];
-  if (transform) {
-    return transformCanvas(ctx, transform.rotate, transform.flip);
-  }
-  return ctx;
+  console.log('orientation', orientation)
+  let transform = EXIF_TRANSFORMS[orientation] || EXIF_TRANSFORMS["1"]
+  return transformCanvas(ctx, transform.rotate, transform.flip)
 }
 
 export function transformCanvas(ctx, degrees = 0, flip = false) {
-  ctx.translate(ctx.canvas.width / 2, ctx.canvas.height / 2);
-  ctx.rotate(degrees);
+  ctx.translate(ctx.canvas.width / 2, ctx.canvas.height / 2)
+  ctx.rotate(degrees)
   if (flip) {
-    ctx.scale(-1, 1);
+    ctx.scale(-1, 1)
   }
-  return ctx;
+  return ctx
 }
